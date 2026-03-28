ebafbbd15f065575bb695aa1ba4fbef26d644bc3
igoramorim <igor340@gmail.com>
2026-03-28 20:20:27 -0300

    zig: move code to src/

--- build.zig
+++ build.zig
@@ -7,7 +7,7 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "zrt",
         .root_module = b.createModule(.{
-            .root_source_file = b.path("main.zig"),
+            .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
         }),
